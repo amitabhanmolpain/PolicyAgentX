5ebde3d7fdee381173c2513f696b39f54c989f4f
Amitabh Anmol Pain <ami05tabh@gmail.com>
2026-07-27 19:09:59 +0530

    feat: bug fix

--- backend/run.py
+++ backend/run.py
@@ -2,5 +2,5 @@ from app import app
 
 
 if __name__ == "__main__":
-    print("Starting PolicyAgentX Backend on http://0.0.0.0:5000")
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    print("Starting PolicyAgentX Backend on http://0.0.0.0:5001")
+    app.run(host="0.0.0.0", port=5001, debug=True)
